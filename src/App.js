9e0664f0fb97edec6f723f10ec5a90edc1e9d945
sjwhole <bluemoon2718@naver.com>
2022-01-15 00:52:10 +0900

    feat: Add route

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import TownPage from "./pages/TownPage";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, useRoutes } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
 import { getAccessToken } from "./api/Auth";
 import request from "./api/axios";
+import { getMe } from "./api/User";
+import { useDispatch, useSelector } from "react-redux";
+import { logIn } from "./actions/authAction";
+import routes from "./routes";
 
 const App = () => {
-  const [login, setLogin] = useState(false);
+  const dispatch = useDispatch();
+  const { loggedIn } = useSelector((state) => state.auth);
 
   useEffect(() => {
     const token = getAccessToken();
+    request.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     if (token) {
-      setLogin(true);
-      request.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      (async () => {
+        const { status, user } = await getMe(token);
+        if (status) {
+          dispatch(logIn(user));
+        }
+      })();
     }
+    console.log(`loggedIn`, loggedIn);
   }, []);
 
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/register" element={<RegisterPage />} />
-          <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/home" element={<TownPage />} />
-        </Routes>
-      </BrowserRouter>
+      {useRoutes(routes(loggedIn))}
     </>
   );
 };
